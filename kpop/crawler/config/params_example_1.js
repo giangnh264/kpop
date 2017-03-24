7718d82e8dff2c8427a740767edc8acfec51ff5d
giangnh <giangnh2@vega.com.vn>
2017-03-24 16:53:30 +0700

    cm

---
+++ kpop/crawler/config/params_example_1.js
@@ -0,0 +1,17 @@
+var params = {
+	host	: 'localhost',
+	user	: 'root',
+	password	: '',
+	database	: 'kpop',
+
+	/*storage: '/var/www/static/img',
+	storage_url: 'http://104.199.237.187:8080/img',*/
+
+	storage: 'E:/Project/kpop_git/static/img',
+	storage_url: 'http://192.168.84.95:8181/img',
+	DS:'/',
+	posts_prepare: 'posts_prepare',
+	posts: 'posts',
+	category: 'category'
+}
+module.exports = params;
