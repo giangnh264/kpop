ef6467479a82cefac2bbbd1d859567a60ec994f8
koigiang <giangnh.264@gmail.com>
2017-03-25 19:29:40 +0700

    commit

--- kpop/protected/commands/shell/crawler_index.sh
+++ kpop/protected/commands/shell/crawler_index.sh
@@ -5,7 +5,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 count=`ps axu | grep crawler_index.sh | grep -v "grep" |wc -l`
 if [ $count -ge 3  ] ; then
     echo "Service is running"
-    /bin/ps -ef |grep "/var/www/kpop/kpop/kpop/protected/commands/shell/crawler_index.sh" |grep -v grep |awk '{print$2}' |xargs kill >/dev/null 2>&1
+    /bin/ps -ef |grep "/var/www/project/kpop/kpop/protected/commands/shell/crawler_index.sh" |grep -v grep |awk '{print$2}' |xargs kill >/dev/null 2>&1
     echo "Service to be killed"
     exit 1
 fi
--- kpop/protected/commands/shell/crawler_pic_video.sh
+++ kpop/protected/commands/shell/crawler_pic_video.sh
@@ -5,9 +5,9 @@ SCRIPTPATH=`dirname $SCRIPT`
 count=`ps axu | grep crawler_pic_video.sh | grep -v "grep" |wc -l`
 if [ $count -ge 3  ] ; then
     echo "Service is running"
-    /bin/ps -ef |grep "/var/www/kpop/kpop/kpop/protected/commands/shell/crawler_pic_video.sh" |grep -v grep |awk '{print$2}' |xargs kill >/dev/null 2>&1
+    /bin/ps -ef |grep "/var/www/project/kpop/kpop/protected/commands/shell/crawler_pic_video.sh" |grep -v grep |awk '{print$2}' |xargs kill >/dev/null 2>&1
     echo "Service to be killed"
     exit 1
 fi
 cd /var/www/project/kpop/kpop/crawler/run
-node crawl_url.js
+node crawl_pic_video.js
